f14b151e6f10c8eebf8d94f8d75c207db4c74104
Alen Thomas <alenthomas9@gmail.com>
2017-08-21 22:45:02 +0530

    implement sqlite db functions to create table, insert into table and select data from table

---
+++ db.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+def get_ids():
+    db_data = []
+    conn = sqlite3.connect("showHN.db")
+    cur = conn.cursor()
+    for row in cur.execute("""SELECT * FROM show_hn"""):
+        db_data.append(row[0])
+    conn.close()
+    return db_data
+
+def insert_id(id_string):
+    conn = sqlite3.connect("showHN.db")
+    cur = conn.cursor()
+    cur.execute("""INSERT INTO show_hn VALUES ({})""".format(id_string))
+    conn.commit()
+    conn.close()
+
+def create_table_show_hn():
+    conn = sqlite3.connect("showHN.db")
+    cur = conn.cursor()
+    cur.execute("""CREATE TABLE show_hn (objectid varchar(20) UNIQUE NOT NULL)""")
+    conn.commit()
+    conn.close()
