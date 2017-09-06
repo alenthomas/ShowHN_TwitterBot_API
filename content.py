b039230251effd92d6e3408383e18e1107dc0df2
Alen Thomas <alenthomas9@gmail.com>
2017-09-06 14:33:35 +0530

    HTML contents are specified in this

---
+++ content.py
@@ -0,0 +1,19 @@
+index_html = """
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Index Page for React App!</title>
+    <link rel="stylesheet" type="text/css" href="css/index.css"
+  </head>
+  <body>
+    <div id="react-app"></div>
+
+    <script src="js/react.min.js"></script>
+    <script src="js/react-dom.min.js"></script>
+    <script src="js/header.js"></script>
+    <script src="js/footer.js"></script>
+    <script src="js/app.js"></script>
+  </body>
+</html>
+"""
