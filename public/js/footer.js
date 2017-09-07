e39539f83197ce0a181d87c84db6fb1e8af72386
Alen Thomas <alenthomas9@gmail.com>
2017-09-07 12:15:40 +0530

    Adds styling

--- public/js/footer.js
+++ public/js/footer.js
@@ -1,5 +1,5 @@
 // footer component
 const FooterElement = React.createElement('footer', {className: 'footer'},
   React.createElement('span', {className: 'copy-left'}, '©'),
-  React.createElement('span', {}, 'Alen Thomas')
+  React.createElement('span', {}, 'al3nthomas')
 )
