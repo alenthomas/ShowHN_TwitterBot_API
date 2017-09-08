9a0e73a9cf9fa67808524295b5b7f53f28125593
Alen Thomas <alenthomas9@gmail.com>
2017-09-08 11:54:25 +0530

    Adds function to calculate hours past from timestamp; renders in <p>

--- public/js/post_item.js
+++ public/js/post_item.js
@@ -1,5 +1,11 @@
 // list_item component
 
+function diffHours(epoch) {
+  const date = new Date(epoch*1000)
+  const today = new Date()
+  const hoursPast = Math.abs(today - date) / (60*60*1000)
+  return hoursPast.toFixed(0)
+}
 const PostItem = React.createClass({
   propTypes: {
     postObj: React.PropTypes.object.isRequired
@@ -11,7 +17,9 @@ const PostItem = React.createClass({
         React.createElement('p', null, this.props.postObj.title),
         React.createElement('a', {href:this.props.postObj.url},
           this.props.postObj.url
-        )
+        ),
+        React.createElement('p', {className: 'timestamp'},
+          '(posted '+diffHours(this.props.postObj.time)+ ' hours ago)')
       )
     )}
 })
