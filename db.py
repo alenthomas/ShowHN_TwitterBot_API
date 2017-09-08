45a9a7ff22c44cd87b44bbb3e8346538d172847e
Alen Thomas <alenthomas9@gmail.com>
2017-09-08 10:48:51 +0530

    Refactors SQL functions to add tile, url, timestamp

--- db.py
+++ db.py
@@ -9,17 +9,29 @@ def get_ids():
     conn.close()
     return db_data
 
-def insert_id(id_string):
+def insert_row(id_string, title, url, timestamp):
     conn = sqlite3.connect("showHN.db")
     cur = conn.cursor()
-    cur.execute("""INSERT INTO show_hn VALUES ({})""".format(id_string))
+    cur.execute("""INSERT INTO show_hn VALUES (?, ?, ?, ?)""", (id_string, title, url, timestamp))
+    conn.commit()
+    conn.close()
+
+def update_row(id_string, title, url, timestamp):
+    conn = sqlite3.connect("showHN.db")
+    cur = conn.cursor()
+    cur.execute("""UPDATE show_hn SET title = ?, url = ? , time = ? WHERE objectid = ?""", (title, url, timestamp, id_string))
     conn.commit()
     conn.close()
 
 def create_table_show_hn():
     conn = sqlite3.connect("showHN.db")
     cur = conn.cursor()
-    cur.execute("""CREATE TABLE show_hn (objectid varchar(20) UNIQUE NOT NULL)""")
+    cur.execute("""CREATE TABLE show_hn (
+        objectid varchar(20) UNIQUE NOT NULL,
+        title TEXT NOT NULL,
+        url TEXT NOT NULL,
+        time TEXT NOT NULL DEFAULT '00')
+    """)
     conn.commit()
     conn.close()
 
